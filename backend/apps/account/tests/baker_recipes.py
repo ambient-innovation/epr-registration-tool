a4fa9ab597b808c84936544ec3508c31a2765e51
Ibrahim Taaloulou <ibrahim.taaloulou@ambient.digital>
2022-05-06 14:25:20 +0200

    :sparkles: feat: add custom user model and test setup

---
+++ backend/apps/account/tests/baker_recipes.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.hashers import make_password
+
+from model_bakery.recipe import Recipe, seq
+
+SUPER_USER_PASSWORD = 'IamKing789'
+SOME_USER_PASSWORD = 'IamNotKing789'
+
+super_user = Recipe(
+    'account.User',
+    email=seq('superuser', suffix='@local.local'),
+    password=make_password(SUPER_USER_PASSWORD),
+    is_superuser=True,
+    is_staff=True,
+    is_active=True,
+)
+
+user = Recipe(
+    'account.User',
+    email=seq('user', suffix='@local.local'),
+    password=make_password(SOME_USER_PASSWORD),
+)
