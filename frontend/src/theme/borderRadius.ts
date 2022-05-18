917f1276b019c97dcc8e2b0249242b70f94d5e7d
Robin Zachmann <robin.zachmann@ambient.digital>
2022-05-18 09:36:08 +0200

    chore: further customize material theme - add custom spacing - add custom border radius - add custom color - fix incorrect font weight - adjust button component

---
+++ frontend/src/theme/borderRadius.ts
@@ -0,0 +1,10 @@
+import { pxToRemAsString } from '@/theme/utils'
+
+export const borderRadius = [
+  0,
+  pxToRemAsString(2),
+  pxToRemAsString(4),
+  pxToRemAsString(6),
+]
+
+export type BorderRadius = typeof borderRadius
--- frontend/src/theme/theme.ts
+++ frontend/src/theme/theme.ts
@@ -1,18 +1,71 @@
+import { Theme as MaterialUITheme } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 
+import { BorderRadius, borderRadius } from './borderRadius'
 import { breakpoints } from './breakpoints'
 import { palette } from './colorsPalette'
 import { shadows } from './shadows'
+import { spacing } from './spacing'
 import { typography } from './typography'
 
 // Create a theme instance.
 export const theme = createTheme({
   palette,
   shadows,
-  spacing: 2, // spacing={4} => 2 * 4 = 8px
   breakpoints,
-  shape: {
-    borderRadius: 1.25, // raduis will be multiplue by 1.25 px
-  },
   typography,
+  spacing,
+  shape: {
+    borderRadius,
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        // No more ripple, on the whole application 💣!
+        disableRipple: true,
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: borderRadius[2],
+          '&.Mui-focusVisible': {
+            outline: '0.125rem solid #4299E1',
+          },
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        asterisk: {
+          // hide asterisk --> we mark optional fields instead
+          display: 'none',
+        },
+      },
+    },
+  },
 })
+
+// https://mui.com/material-ui/customization/palette/#adding-new-colors
+declare module '@mui/material' {
+  interface Theme {
+    shape: {
+      borderRadius: BorderRadius
+    }
+  }
+  interface ThemeOptions {
+    shape: {
+      borderRadius: BorderRadius
+    }
+  }
+}
+
+/**
+ * note: ALWAYS use the theme like this if you need to import it
+ * import { Theme } from '@mui/material'
+ * **/
+
+// Re-declare the emotion theme to have the properties of the MaterialUiTheme
+// --> make css={(theme) => ...} work with mui theme
+// https://github.com/emotion-js/emotion/discussions/2291#discussioncomment-491185x
+declare module '@emotion/react' {
+  export interface Theme extends MaterialUITheme {}
+}
