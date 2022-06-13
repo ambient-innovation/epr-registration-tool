a3284d36760f4acc0df2bd62e074cc8e5b2a14c4
Bartosz Cupial <bartosz.cupial@ambient.digital>
2022-06-13 12:21:35 +0300

    chore: test only firefox config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -24,16 +24,8 @@ const config: PlaywrightTestConfig = {
   },
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
     },
   ],
 };
