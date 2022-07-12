d915f9afdda5e11c473d6afe7590acab0d6f1023
Robin Zachmann <robin.zachmann@ambient.digital>
2022-07-12 13:27:24 +0200

    chore: remove example e2e test

--- e2e/tests/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  // Go to http://127.0.0.1:3000/
-  await page.goto("http://127.0.0.1:3000/");
-  // Click text=EPR Registration Tool
-  const title = page.locator("text=Welcome!");
-  await expect(title).toContainText("Welcome!");
-});
