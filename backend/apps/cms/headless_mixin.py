125cb0a291c6fae295701bb92f0ed19e5ec91386
Robin Zachmann <robin.zachmann@ambient.digital>
2022-08-01 17:25:27 +0200

    feat(cms): add custom serve url - fixes inline links in rich text - adds links in cms to live version of pages

--- backend/apps/cms/headless_mixin.py
+++ backend/apps/cms/headless_mixin.py
@@ -1,12 +1,15 @@
 from urllib.parse import urljoin
 
 from django.conf import settings
+from django.shortcuts import redirect
 from django.utils.http import urlencode
 
 from wagtail_headless_preview.models import HeadlessMixin
 
 
 class CustomHeadlessMixin(HeadlessMixin):
+    SERVE_PATH = 'go-to'
+
     def get_preview_url(self, token):
         """
         Similar to the original implementation, but:
@@ -24,3 +27,14 @@ class CustomHeadlessMixin(HeadlessMixin):
                 }
             )
         )
+
+    def serve(self, request):
+        """
+        Slightly adjusted version of the HeadlessMixin.serve(),
+        that removes the base bath of the serve URL.
+        This is necessary, because our serve-url does not start at root level.
+        """
+        base_url = settings.FRONTEND_URL
+        site_id, site_root, relative_page_url = self.get_url_parts(request)
+
+        return redirect(f"{base_url.rstrip('/')}{relative_page_url.removeprefix('/' + self.SERVE_PATH)}")
