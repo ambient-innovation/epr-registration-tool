d69b4324bfd9de86b79ac6e51b47a3f1226ab622
ibrahimtaaloulou <ibrahim.taaloulou@ambient.digital>
2022-08-10 10:55:48 +0200

    fix: No receivers for registration complete notification

--- backend/apps/company/email.py
+++ backend/apps/company/email.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from sentry_sdk import capture_message
+from sentry_sdk import capture_message, configure_scope
 
 from common.email import render_email, send_html_email, send_translated_email
 from company.models import Company
@@ -44,15 +44,16 @@ def send_admin_registration_notification(company: Company):
 def send_user_registration_complete_notification(company):
     assert company is not None
     receiver_list = company.users_queryset.all().active()
-
+    if not len(receiver_list):
+        with configure_scope() as scope:
+            scope.set_extra("company_id", company.pk)
+            capture_message('No receivers for registration complete notification')
     for user in receiver_list:
         send_translated_email(
             'registration_complete',
             user,
             url=urljoin(settings.FRONTEND_URL, '/auth/login'),
         )
-    else:
-        capture_message(f'No receivers for registration complete notification (company ID={company.id})')
 
 
 def send_company_data_changed_notification(company):
